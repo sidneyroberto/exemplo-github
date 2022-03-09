a5b9b7de9b959031598a71be81101dbdc0f153fa
Sidney Sousa <sidney.sousa@ifms.edu.br>
2022-03-08 21:59:26 -0400

    estilizacao quase finalizada

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-import './style.css'
+const $ = document.querySelector.bind(document)
+
+const addTask = (event) => {
+  event.preventDefault()
+  const taskDescription = $('#task-description').value
+
+  const newTaskItem = `
+    <li>${taskDescription}</li>
+  `
+
+  $('#task-list').insertAdjacentHTML('beforeend', newTaskItem)
+  $('#task-form').reset()
+}
